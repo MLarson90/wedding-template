d14dc00a659fe87f304b7e5e1000da548862573f
Max Larson <mr.m.larson90@gmail.com>
2017-06-13 15:36:34 -0700

    create rsvp object from user input

--- src/app/rsvp.model.ts
+++ src/app/rsvp.model.ts
@@ -1,3 +1,3 @@
 export class Rsvp {
-  constructor(public first: string, public last: string, public datename: string, public kids: number,  public wedding: number, public reception: number, public brunch: number, public hotel: number) {}
+  constructor(public first: string, public last: string, public datefirst: string, public datelast: string, public kids: number,  public wedding: number, public reception: number, public brunch: number, public hotel: number) {}
 }
--- src/app/rsvp/rsvp.component.ts
+++ src/app/rsvp/rsvp.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Rsvp } from '../rsvp.model';
+
 declare var $:any;
 @Component({
   selector: 'app-rsvp',
@@ -7,14 +9,40 @@ declare var $:any;
 })
 export class RsvpComponent implements OnInit {
   submit(first:string,last:string,datefirst:string,datelast:string,numberOfKids:number,reception:string,brunch:string,hotel:string){
-    console.log(first);
-    console.log(last);
-    console.log(datefirst);
-    console.log(datelast);
-    console.log(numberOfKids);
-    console.log(reception);
-    console.log(brunch);
-    console.log(hotel);
+    //calculate how many adults are coming (1 or 2)
+    var date = function(datefirst){
+      if(datefirst){
+        return 2;
+      }else{
+        return 1;
+      }
+    }
+    //calculate number of children attending
+    var kids = function(numberOfKids){
+      if(numberOfKids){
+        return parseInt(numberOfKids);
+      }else{
+        return 0;
+      }
+    }
+    //adult and total party totals
+    var adults = date(datefirst);
+    var party = adults + kids(numberOfKids);
+    //how many attending each event
+    var comingToEvent = function(coming, party){
+      var reception = 0;
+      if(coming === 'true'){
+        reception += party;
+      }
+      return reception;
+      }
+    //event totals
+    var receptionNumber = comingToEvent(reception, party);
+    var brunchNumber = comingToEvent(brunch, party);
+    var hotelNumber = comingToEvent(hotel, party);
+
+    var totalsForDb = new Rsvp(first,last, datefirst, datelast, numberOfKids, party, receptionNumber, brunchNumber, hotelNumber);
+    console.log(totalsForDb);
   }
   constructor() { }
 
