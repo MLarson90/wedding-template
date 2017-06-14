b9af544d6689401464664e00ce4be881fdf8ef84
max <mr.m.larson90@gmail.com>
2017-06-14 16:45:06 -0700

    add notes on admin componenet

--- src/app/admin/admin.component.ts
+++ src/app/admin/admin.component.ts
@@ -12,6 +12,7 @@ import { Observable } from 'rxjs/Rx';
   providers:[RsvpService]
 })
 export class AdminComponent implements OnInit {
+  //place holders to store values from below functions to display on screen
   rsvp;
   wedding;
   weddingTotal = 0;
@@ -26,25 +27,31 @@ export class AdminComponent implements OnInit {
   constructor(private Service: RsvpService) { }
 
   ngOnInit() {
+    // display all guest names
     this.rsvp = this.Service.getGuest();
+    //display number of guest attending wedding
     this.wedding = this.Service.getGuest().subscribe(result => {for(let i = 0; i < result.length; i++) {
         this.weddingTotal += result[i].wedding;
       }
     });
+    //display number of guest attending reception
     this.reception = this.Service.getGuest().subscribe(result => {for(let i = 0; i < result.length; i++) {
         this.receptionTotal += result[i].reception;
       }
     });
+    //display number of guest attending brunch
     this.brunch = this.Service.getGuest().subscribe(result => {for(let i = 0; i < result.length; i++) {
         this.brunchTotal += result[i].brunch;
       }
     });
+    //displays number of kids attending wedding
     this.kids = this.Service.getGuest().subscribe(result => {for(let i = 0; i < result.length; i++) {
         if(result[i].kids!= ""){
         this.kidTotal += result[i].kids;
       }
       }
     });
+    //displays number of guest staying at recomended hotel
     this.hotel = this.Service.getGuest().subscribe(result => {for(let i = 0; i < result.length; i++) {
         if(result[i].hotel!= ""){
         this.hotelTotal += result[i].hotel;
