b3736c9e6f8e81f448c355318beed249ed818a72
Max <mr.m.larson90@gmail.com>
2017-06-15 13:18:39 -0700

    add beginng to comment page with style

--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -1,15 +1,24 @@
 import { Component, OnInit } from '@angular/core';
+import { Router, RouterModule } from '@angular/router';
+import { RsvpService } from 'app/rsvp.service';
+import { Comment } from '../comment.model';
 
 @Component({
   selector: 'app-contact',
   templateUrl: './contact.component.html',
-  styleUrls: ['./contact.component.css']
+  styleUrls: ['./contact.component.css'],
+  providers: [RsvpService]
 })
 export class ContactComponent implements OnInit {
 
-  constructor() { }
-
+  constructor(private rsvp:RsvpService, private router: Router) { }
+    submit(name:string, said:string){
+      var comment = new Comment(name, said);
+      this.rsvp.addComment(comment);
+    }
+    comments;
   ngOnInit() {
+    this.comments = this.rsvp.getComment();
   }
 
 }
--- src/app/rsvp.service.ts
+++ src/app/rsvp.service.ts
@@ -1,13 +1,16 @@
 import { Injectable } from '@angular/core';
 import { Rsvp } from './rsvp.model';
+import { Comment } from './comment.model';
 import { AngularFireDatabase, FirebaseListObservable } from 'angularfire2/database';
 import { Observable } from 'rxjs/Rx';
 
 @Injectable()
 export class RsvpService {
   rsvp: FirebaseListObservable<any[]>;
+  comment: FirebaseListObservable<any[]>;
   constructor(private database: AngularFireDatabase, af: AngularFireDatabase) {
     this.rsvp = database.list('guests');
+    this.comment = database.list('comments');
   }
   getGuest(){
     return this.rsvp;
@@ -16,5 +19,10 @@ export class RsvpService {
   addGuest(newguest: Rsvp){
     this.rsvp.push(newguest);
   }
-
+  getComment(){
+    return this.comment;
+  }
+  addComment(newcomment: Comment){
+    this.comment.push(newcomment);
+  }
 }
