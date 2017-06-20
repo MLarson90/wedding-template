374c4b03ea38b353d78cffb12e3be269825911d4
Max <mr.m.larson90@gamil.com>
2017-06-20 13:35:35 -0700

    add more phone styles

--- src/app/api-keys.ts
+++ src/app/api-keys.ts
@@ -1,8 +1,8 @@
 export var masterFirebaseConfig = {
-  apiKey: "AIzaSyBj3bVP0n4AaTYfM5J8VPZ5mlAWahCQL54",
-  authDomain: "wedding-1fde5.firebaseapp.com",
-  databaseURL: "https://wedding-1fde5.firebaseio.com",
-  projectId: "wedding-1fde5",
-  storageBucket: "wedding-1fde5.appspot.com",
-  messagingSenderId: "713343180215"
+apiKey: "AIzaSyABVrsRjJlEiSnBHWneGJErDl8gQyAcLOc",
+    authDomain: "wedding-t.firebaseapp.com",
+    databaseURL: "https://wedding-t.firebaseio.com",
+    projectId: "wedding-t",
+    storageBucket: "wedding-t.appspot.com",
+    messagingSenderId: "547343917175"
 }
